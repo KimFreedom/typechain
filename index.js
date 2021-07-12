380df90abb4dde904da7e20b5fc974aaa7b2f373
Kim Freedom <glay777@hanmail.net>
2021-07-12 19:45:45 +0900

    First steps with Typescript

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("hello");
+"use strict";
+exports.__esModule = true;
+var name = "Freedom", age = 37, gender = "mail";
+var sayHi = function (name, age, gender) {
+    console.log("Hello " + name + ", you are " + age + ", you are a " + gender);
+};
+sayHi(name, age);
